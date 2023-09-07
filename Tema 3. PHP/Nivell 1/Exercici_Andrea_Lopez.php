ecfc15c033bad2de58d5c7dc029a6e40b595f5a1
AndreaLN25 <andrealn14@gmail.com>
2023-09-07 16:16:35 +0200

    Exercici1

---
+++ Tema 3. PHP/Nivell 1/Exercici_Andrea_Lopez.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $dia = 6;
+    $preu = 123.45;
+    $nom = "Andrea";
+    $avui_plou = true;
+
+    echo "Integer: " .$dia;
+    echo "Double: " .$preu;
+    echo "String:" .$nom;
+    echo "Boolean: " ($avui_plou ? "true" : "false");
+    ?>
+
+</body>
+</html>
