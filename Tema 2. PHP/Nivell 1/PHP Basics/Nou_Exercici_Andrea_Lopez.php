72a1d73670e53e48727e79d19cd96cd5bb316755
AndreaLN25 <andrealn14@gmail.com>
2023-10-03 15:09:09 +0200

    ex4t2

--- Tema 2. PHP/Nivell 1/PHP Basics/Nou_Exercici_Andrea_Lopez.php
+++ Tema 2. PHP/Nivell 1/PHP Basics/Nou_Exercici_Andrea_Lopez.php
@@ -77,5 +77,18 @@ $operacio = readline ("Indica l'operació a realitzar:");
 }
 
 
+/*Exercici 4-Fes un programa que implementi una funció on es compti fins a un nombre determinat. 
+Si no s’inclou un nombre determinat, el nombre haurà de tenir un valor per defecte igual a 10. 
+A més, aquesta funció ha de tenir un segon paràmetre que indiqui de quant a quant es compta(D'1 en 1, de 2 en 2…). 
+El compte s’ha de mostrar per pantalla pas per pas.*/
+
+function comptar($final = 10,$interval){
+    
+    for ($i = 1; $i <= $final; $i += $interval){
+        echo $i;
+    }
+}
+
+
 
 ?>
