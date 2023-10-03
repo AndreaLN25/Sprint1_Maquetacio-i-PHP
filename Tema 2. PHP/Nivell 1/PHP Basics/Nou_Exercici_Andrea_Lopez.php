f1d88d767c08e2873d470d8c8ce4d55209065fb1
AndreaLN25 <andrealn14@gmail.com>
2023-10-03 12:03:41 +0200

    ex2T2_NOU

--- Tema 2. PHP/Nivell 1/PHP Basics/Nou_Exercici_Andrea_Lopez.php
+++ Tema 2. PHP/Nivell 1/PHP Basics/Nou_Exercici_Andrea_Lopez.php
@@ -11,3 +11,20 @@ echo "Double: " .$preu;
 echo "String:" .$nom;
 echo "Boolean: " ($avui_plou ? "true" : "false");
 
+//Exercici2
+//Imprimeix per pantalla "Hello, World!" utilitzant una variable.
+$salutacio = "Hello, World!"; 
+echo $salutacio;
+//Converteix tots els caràcters de l'string a majúscules i imprimeix en pantalla.
+$SALUTACIO = strtoupper($salutacio);
+echo $SALUTACIO; 
+//Imprimeix per pantalla la mida (longitud) de la variable.
+$longitud = strlen($salutacio);
+echo "El missatge té " .$longitud ."caracters";
+//Imprimeix per pantalla l'string en ordre invers de caràcters.
+$salutacioInversa = strrev($salutacio);
+echo "Escribim el missage al invers: " .$salutacioInversa;
+//Crea una nova variable amb el contingut "Aquest és el curs de PHP" i imprimeix per pantalla la concatenació de tots dos strings.
+$informacio = "Aquest és el curs de PHP";
+echo $salutacio . $informacio; 
+
