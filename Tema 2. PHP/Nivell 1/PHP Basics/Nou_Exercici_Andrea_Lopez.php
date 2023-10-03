a683b98412c93b7483bc76eb34a9f743603e946f
AndreaLN25 <andrealn14@gmail.com>
2023-10-03 15:10:46 +0200

    ex6t2_NOU

--- Tema 2. PHP/Nivell 1/PHP Basics/Nou_Exercici_Andrea_Lopez.php
+++ Tema 2. PHP/Nivell 1/PHP Basics/Nou_Exercici_Andrea_Lopez.php
@@ -110,6 +110,21 @@ function comptar($final = 10,$interval){
             echo "Ha hagut algun error";
         }
     }
+    
+        
+//Exercici6-
+    /*Charlie em va mossegar el dit!
+    Charlie et mossegarà el dit exactament el 50% del temps.
+    Escriu La funció isBitten () que retorna TRUE amb un 50% de probabilitat i FALSE en cas contrari.*/
+
+    function isBitten(){
+        $mossegar = rand(1,2);
+        if ($mossegar ==1) {
+            $mossegat = "true";
+        }else {
+            $mossegat = "false";
+        }
+    }
 
 
 
