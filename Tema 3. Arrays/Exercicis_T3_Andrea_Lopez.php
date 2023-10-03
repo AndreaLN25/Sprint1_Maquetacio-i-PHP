f588e83d139868768603596a39f53410b201665c
AndreaLN25 <andrealn14@gmail.com>
2023-10-03 16:15:42 +0200

    Ex1T3

---
+++ Tema 3. Arrays/Exercicis_T3_Andrea_Lopez.php
@@ -0,0 +1,22 @@
+<?php
+//Exercici 1- Crea un array, afegeix-li 5 nombres enters i després mostrals per pantalla d’un en un.
+//opcio1
+$nombre = $nombresEnters(0,1,2,3,4);
+foreach ($nombresEnters as $nombre) {
+    echo $nombre;
+}
+
+//opcio2
+$nombresEnters2 = [0,1,2,3,4];
+var_dump ($nombresEnters2);
+
+
+
+
+
+
+
+
+?>
+
+
