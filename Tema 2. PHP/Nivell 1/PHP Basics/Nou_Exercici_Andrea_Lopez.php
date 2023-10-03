3d2dea01a8c0ccbac1a3e5007545ab469e099b59
AndreaLN25 <andrealn14@gmail.com>
2023-10-03 12:03:05 +0200

    Ex1T2_nou

---
+++ Tema 2. PHP/Nivell 1/PHP Basics/Nou_Exercici_Andrea_Lopez.php
@@ -0,0 +1,13 @@
+<?php
+
+//Exercici1-Defineix una variable de cada tipus: integer,double,string i boolean. Després, imprimeix-les per pantalla.
+$dia = 6;
+$preu = 123.45;
+$nom = "Andrea";
+$avui_plou = true;
+
+echo "Integer: " .$dia;
+echo "Double: " .$preu;
+echo "String:" .$nom;
+echo "Boolean: " ($avui_plou ? "true" : "false");
+
