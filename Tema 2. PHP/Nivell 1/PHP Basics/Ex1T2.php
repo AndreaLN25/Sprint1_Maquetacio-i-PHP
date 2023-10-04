4cd33c529b19e2a01deced51b48216d977bbb67b
AndreaLN25 <andrealn14@gmail.com>
2023-10-04 17:33:37 +0200

    ex1 amb constant

--- Tema 2. PHP/Nivell 1/PHP Basics/Ex1T2.php
+++ Tema 2. PHP/Nivell 1/PHP Basics/Ex1T2.php
@@ -1,15 +1,19 @@
 <?php
 
 //Exercici1-Defineix una variable de cada tipus: integer,double,string i boolean. Després, imprimeix-les per pantalla.
+//Després crea una constant que inclogui el teu nom i mostra-ho en format títol per pantalla.
+
 echo "\n Exercici 1 \n";
+
 $dia = 6;
 $preu = 123.45;
 $nom = "Andrea";
 $avui_plou = true;
-
+define("NOM", "Andrea");
 echo "Integer: " .$dia. "\n";
 echo "Double: " .$preu. "\n";
 echo "String:" .$nom. "\n";
 echo "Boolean: " .($avui_plou ? "true" : "false"). "\n";
+echo NOM;
 
 ?>
