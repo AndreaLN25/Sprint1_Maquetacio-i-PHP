106ab0adce3eace0964679195f7a370ba4210a1c
AndreaLN25 <andrealn14@gmail.com>
2023-10-05 13:04:45 +0200

    moviment carpetes -enunciats x aturada moodle

---
+++ Tema 6. PHP avançat/Nivell1/Ex1T6.php
@@ -0,0 +1,9 @@
+<?php
+
+
+/*
+
+- Exercici 1
+Crea un formulari HTML amb els camps que vulguis (almenys un nom o username). El formulari ha de tenir com a action un document PHP. El codi d’aquest document PHP haurà de mostrar els valors dels diferents camps del formulari mitjançant variables superglobals.
+*/
+?>
---
+++ Tema 6. PHP avançat/Nivell1/Ex2T6.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+- Exercici 2
+Millora l’exercici anterior fent que es guardi l’username en una variable de sessió. S’haurà de mostrar l’username per pantalla mitjançant l’accés a aquesta variable de sessió.
+*/
+
+?>
