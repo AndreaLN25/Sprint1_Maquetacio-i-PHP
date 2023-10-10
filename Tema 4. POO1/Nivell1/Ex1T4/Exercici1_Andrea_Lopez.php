d3054d1644cbd270f6fd9cb189ee6bc5409b5ee1
AndreaLN25 <andrealn14@gmail.com>
2023-10-10 19:28:10 +0200

    canvi funcio print

--- Tema 4. POO1/Nivell1/Ex1T4/Exercici1_Andrea_Lopez.php
+++ Tema 4. POO1/Nivell1/Ex1T4/Exercici1_Andrea_Lopez.php
@@ -1,38 +1,37 @@
-    <?php
+<?php
+
 //Exercici 1- 
 /*Crea una classe Employee, defineix com a atributs el seu nom i sou.
 Definir un mètode initialize que rebi com a paràmetres el nom i sou. 
-Plantejar un segon mètode print que imprimeixi el nom i un missatge
+Plantejar un segon mètode print que imprimeixi el nom i un missatge-------------------
     si ha de pagar o no impostos (si el sou supera 6000, paga impostos).*/
 
 
-class Employee {
-    private $nom;
-    private $sou;
+    class Employee{
+        protected $nom;
+        protected $sou; 
 
-
-    function initialize($nom,$sou) {
-        $this->nom = $nom;
-        $this->sou = $sou;
-
-    }
-
-    function __toString(){
-        if ($this->sou > 6000){
-            return " hola, $this->nom, Hauràs de pagar impostos";
-        }else{
-            return " hola, $this->nom, No pagaràs impostos";
+        public function initialize($nom,$sou){
+            $this->nom=$nom;
+            $this->sou=$sou;
         }
+
+        public function paguesImpostos(){
+            if ($this->sou>6000){
+                echo "Hola $this->nom pagaràs impostos";
+            }else{
+                echo "hola $this->nom no pagaràs impostos";
+            }
+
+        }
+
     }
-}
 
+    //main
 
-$Empleat1 = new Employee;
-$Empleat1 ->initialize("Andrea", 10000);
-echo $Empleat1->__toString();
+    $empleat1= new Employee;
+    $empleat1->initialize("pep", 1000);
+    echo $empleat1->paguesImpostos();
 
-
-    ?>
-
-
+?>
 
