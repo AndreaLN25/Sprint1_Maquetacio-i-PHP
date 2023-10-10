a01b43ca3c743ec4e80b71ca5684fcf338530ce3
AndreaLN25 <andrealn14@gmail.com>
2023-10-10 13:12:20 +0200

    carpetes i enunciats

--- Tema 6. PHP avançat/Nivell1/Ex2T6.php
+++ Tema 6. PHP avançat/Nivell1/Ex2T6.php
@@ -1,7 +1,6 @@
 <?php
 
-/*
-- Exercici 2
+/*Tema6-Exercici 2-Nivel1
 Millora l’exercici anterior fent que es guardi l’username en una variable de sessió. S’haurà de mostrar l’username per pantalla mitjançant l’accés a aquesta variable de sessió.
 */
 
