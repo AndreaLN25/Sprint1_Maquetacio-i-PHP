7ade361f66819e320113c0ac6a0ab5a5a14fd62a
AndreaLN25 <andrealn14@gmail.com>
2023-10-24 13:12:21 +0200

    PokerDice (T4-N2 POO)

---
+++ Tema 4. POO1/Nivell2/PokerDice.php
@@ -0,0 +1,40 @@
+<?php
+
+/*Exercici 1- Crea la classe PokerDice. Les cares d'un dau de pòquer tenen les següents figures: As, K, Q, J, 7 i 8.
+
+Crea el mètode throw que no fa altra cosa que tirar el dau, és a dir, genera un valor aleatori per a l'objecte a què se li aplica el mètode.
+
+Crea també el mètode shapeName, que digui quina és la figura que ha sortit en l'última tirada del dau en qüestió.
+
+Realitza una aplicació que permeti tirar cinc daus de pòquer alhora.
+
+A més, programa el mètode getTotalThrows que ha de mostrar el nombre total de tirades entre tots els daus.*/
+
+
+class PokerDice {
+    private $cares = ["As", "K", "Q", "J", 7, 8];
+    private $ultimaTiradaDau;
+    private $totalTirades = 0; 
+
+    public function throwDau(){
+        $tiradaAleatoria = array_rand($this->cares,1);
+        $this->ultimaTiradaDau = $this->cares[$tiradaAleatoria];
+        $this->totalTirades++;
+    }
+
+    public function shapeName(){
+        return $this->ultimaTiradaDau;
+    }
+
+    public function getTotalTirades(){
+        return $this ->totalTirades; 
+    }
+    
+}
+
+/*$dau = new PokerDice();
+$dau -> throwDau();
+echo "last throw " . $dau->shapeName() . "\n"; 
+echo "tirades " . $dau ->getTotalTirades(). "\n"; */
+
+?>
